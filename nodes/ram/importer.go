4f6319674110e6ef32395acf5f560faefa03177a
awgh <awgh@awgh.org>
2016-10-22 00:20:18 -0700

    Added support for de/serialization of content and routing key types (requires matched changed in bencrypt)

--- nodes/ram/importer.go
+++ nodes/ram/importer.go
@@ -2,7 +2,9 @@ package ram
 
 import (
 	"encoding/json"
+	"errors"
 
+	"github.com/awgh/bencrypt"
 	"github.com/awgh/ratnet"
 	"github.com/awgh/ratnet/api"
 )
@@ -18,25 +20,29 @@ type channelPrivB64 struct {
 }
 
 type exportedNode struct {
-	ContentKey string
-	RoutingKey string
-	Profiles   []profilePrivB64
-	Contacts   []api.Contact
-	Channels   []channelPrivB64
-	Peers      []api.Peer
-	Router     api.Router
-	Policies   []api.Policy
+	ContentKey  string
+	ContentType string
+	RoutingKey  string
+	RoutingType string
+	Profiles    []profilePrivB64
+	Contacts    []api.Contact
+	Channels    []channelPrivB64
+	Peers       []api.Peer
+	Router      api.Router
+	Policies    []api.Policy
 }
 
 type importedNode struct {
-	ContentKey string
-	RoutingKey string
-	Profiles   []profilePrivB64
-	Contacts   []api.Contact
-	Channels   []channelPrivB64
-	Peers      []api.Peer
-	Router     map[string]interface{}
-	Policies   []map[string]interface{}
+	ContentKey  string
+	ContentType string
+	RoutingKey  string
+	RoutingType string
+	Profiles    []profilePrivB64
+	Contacts    []api.Contact
+	Channels    []channelPrivB64
+	Peers       []api.Peer
+	Router      map[string]interface{}
+	Policies    []map[string]interface{}
 }
 
 // Import : Load a node configuration from a JSON config
@@ -50,6 +56,18 @@ func (node *Node) Import(jsonConfig []byte) error {
 	if err := json.Unmarshal(jsonConfig, &nj); err != nil {
 		return err
 	}
+	// setup content and routing keys
+	v, ok := bencrypt.KeypairTypes[nj.ContentType]
+	if !ok {
+		return errors.New("Unknown Content Keypair Type in Import")
+	}
+	node.contentKey = v()
+	v, ok = bencrypt.KeypairTypes[nj.RoutingType]
+	if !ok {
+		return errors.New("Unknown Routing Keypair Type in Import")
+	}
+	node.routingKey = v()
+
 	if len(nj.ContentKey) > 0 {
 		if err := node.contentKey.FromB64(nj.ContentKey); err != nil {
 			return err
@@ -100,7 +118,9 @@ func (node *Node) Import(jsonConfig []byte) error {
 func (node *Node) Export() ([]byte, error) {
 	var nj exportedNode
 	nj.ContentKey = node.contentKey.ToB64()
+	nj.ContentType = node.contentKey.GetName()
 	nj.RoutingKey = node.routingKey.ToB64()
+	nj.RoutingType = node.routingKey.GetName()
 	nj.Channels = make([]channelPrivB64, len(node.channels))
 	i := 0
 	for _, v := range node.channels {
@@ -129,46 +149,51 @@ func (node *Node) Export() ([]byte, error) {
 }
 
 /* Sample JSON for a node configuration
-{
-	"ContentKey": "JsQTwSsZW4srWuX+9iCi5SRCulXSWo3xwFIfbu3y9gtIIUmk8fzloo0Nik1R88mSpJ8ODsn9NzWJ22VQ/xtnnw==",
-	"RoutingKey": "b6f1o1e51JvMmoerJKWI47ZbYSTO+Pi03dOXvZCYzGbsVJbuoEmqo48Wnxag2GzCVeOrtJZS02jT5Nq3jrpQgQ==",
-	"Profiles": [],
-	"Contacts": [],
-	"Channels": [],
-	"Peers": null,
-	"Router": {
-		"CheckChannels": true,
-		"CheckContent": true,
-		"CheckProfiles": false,
-		"ForwardConsumedChannels": true,
-		"ForwardConsumedContent": false,
-		"ForwardConsumedProfiles": false,
-		"ForwardUnknownChannels": true,
-		"ForwardUnknownContent": true,
-		"ForwardUnknownProfiles": false,
-		"Patches": {},
-		"type": "default"
-	},
-	"Policies": [ {
-		"AdminMode": false,
-		"ListenURI": ":20001",
-		"Transport": {
-			"Certfile": "cert.pem",
-			"EccMode": true,
-			"Keyfile": "key.pem",
-			"type": "https"
-		},
-		"type": "server"
-	}, {
-		"AdminMode": true,
-		"ListenURI": "localhost:20002",
-		"Transport": {
-			"Certfile": "cert.pem",
-			"EccMode": true,
-			"Keyfile": "key.pem",
-			"type": "https"
-		},
-		"type": "server"
-	} ]
+ {
+    "ContentKey": "JsQTwSsZW4srWuX+9iCi5SRCulXSWo3xwFIfbu3y9gtIIUmk8fzloo0Nik1R88mSpJ8ODsn9NzWJ22VQ/xtnnw==",
+    "ContentType": "Curve25519,AES-CBC-256,HMAC-SHA-256",
+    "RoutingKey": "b6f1o1e51JvMmoerJKWI47ZbYSTO+Pi03dOXvZCYzGbsVJbuoEmqo48Wnxag2GzCVeOrtJZS02jT5Nq3jrpQgQ==",
+    "RoutingType": "Curve25519,AES-CBC-256,HMAC-SHA-256",
+    "Profiles": [],
+    "Contacts": [],
+    "Channels": [],
+    "Peers": null,
+    "Router": {
+        "CheckChannels": true,
+        "CheckContent": true,
+        "CheckProfiles": false,
+        "ForwardConsumedChannels": true,
+        "ForwardConsumedContent": false,
+        "ForwardConsumedProfiles": false,
+        "ForwardUnknownChannels": true,
+        "ForwardUnknownContent": true,
+        "ForwardUnknownProfiles": false,
+        "Patches": {},
+        "type": "default"
+    },
+    "Policies": [
+        {
+            "AdminMode": false,
+            "ListenURI": ":20001",
+            "Transport": {
+                "Certfile": "cert.pem",
+                "EccMode": true,
+                "Keyfile": "key.pem",
+                "type": "https"
+            },
+            "type": "server"
+        },
+        {
+            "AdminMode": true,
+            "ListenURI": "localhost:20002",
+            "Transport": {
+                "Certfile": "cert.pem",
+                "EccMode": true,
+                "Keyfile": "key.pem",
+                "type": "https"
+            },
+            "type": "server"
+        }
+    ]
 }
 */
