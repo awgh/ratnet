f636366ba9392e3574574f4043640d20027b80a0
awgh <awgh@awgh.org>
2018-02-10 18:39:06 -0800

    MAJOR OVERHAUL
    Tons of optimizations and fixes, minor breaking changes.  In some tests, these changes showed a 20x throughput improvement.
    
    - UDP and HTTPS transports have switched from the JSON encoding, which base64'ed everything, to Gob encoding
    - RPC interface has been totally refactored to require fewer encode/decode operations (although client code will have to do type assertions now -> breaking)
    - QLDB has been modified to store outbox messages in binary format as blob instead of base64 enc/dec that field (will break with old databases)
    - pollServer, which is shared by p2p and poll has been moved to policy/common.go and been cleaned up with new RPC changes
    - PublicRPC and AdminRPC have been refactored and moved to nodes/rpc.go, where both the RamNode and QlNode can share them.  Future nodes can still customize this.
    - type_registry is the new home for adding any types to the global Gob encoder
    - More unit tests have been added to get coverage over all possible RPC return types
    - QLDB has had "Bulk" operations added, for bulk sending of messages at once (for example, sending an entire large file)... might add this to RamNode, too
    
    In general, most things are cleaner and a lot of crap code was deleted.
    
    This is a "if I get hit by a bus" check-in, so not all print statements have been cleaned up yet.

---
+++ nodes/rpc.go
@@ -0,0 +1,327 @@
+package nodes
+
+import (
+	"errors"
+	"log"
+	"strconv"
+
+	"github.com/awgh/bencrypt/bc"
+	"github.com/awgh/ratnet/api"
+)
+
+// PublicRPC : Entrypoint for RPC functions that are exposed to the public/Internet
+func PublicRPC(node api.Node, call api.RemoteCall) (interface{}, error) {
+	switch call.Action {
+	case "ID":
+		var i bc.PubKey
+		i, err := node.ID()
+		log.Printf("PublicRPC got %+v : %+v\n", i, err)
+		if err != nil {
+			return nil, err
+		}
+		return i, nil
+
+	case "Pickup":
+		if len(call.Args) < 2 {
+			return nil, errors.New("Invalid argument count")
+		}
+		rpk, ok := call.Args[0].(bc.PubKey)
+		if !ok {
+			return nil, errors.New("Invalid argument 1")
+		}
+		i, ok := call.Args[1].(int64)
+		if !ok {
+			return nil, errors.New("Invalid argument 2")
+		}
+		var xargs []string // dunno how to type-assert slices
+		for _, v := range call.Args[2:] {
+			vs, ok := v.(string)
+			if !ok {
+				return nil, errors.New("Invalid argument 3+")
+			}
+			xargs = append(xargs, vs)
+		}
+		b, err := node.Pickup(rpk, i, xargs...)
+		if err != nil {
+			return nil, err
+		}
+		return b, err
+
+	case "Dropoff":
+		if len(call.Args) < 1 {
+			return nil, errors.New("Invalid argument count")
+		}
+		bundle, ok := call.Args[0].(api.Bundle)
+		if !ok {
+			return nil, errors.New("Invalid argument")
+		}
+		return nil, node.Dropoff(bundle)
+
+	default:
+		return nil, errors.New("No such method: " + call.Action)
+	}
+}
+
+// AdminRPC : Entrypoint for administrative RPC functions that should not be exposed to the Internet
+func AdminRPC(node api.Node, call api.RemoteCall) (interface{}, error) {
+	switch call.Action {
+
+	case "CID":
+		b, err := node.CID()
+		if err != nil {
+			return nil, err
+		}
+		return b, nil
+
+	case "GetContact":
+		if len(call.Args) < 1 {
+			return nil, errors.New("Invalid argument count")
+		}
+		contactName, ok := call.Args[0].(string)
+		if !ok {
+			return nil, errors.New("Invalid argument")
+		}
+		if peer, err := node.GetContact(contactName); err != nil {
+			return nil, err
+		} else {
+			return peer, nil
+		}
+
+	case "GetContacts":
+		if peers, err := node.GetContacts(); err != nil {
+			return nil, err
+		} else {
+			return peers, nil
+		}
+
+	case "AddContact":
+		if len(call.Args) < 2 {
+			return nil, errors.New("Invalid argument count")
+		}
+		contactName, ok := call.Args[0].(string)
+		if !ok {
+			return nil, errors.New("Invalid argument")
+		}
+		contactB64Key, ok := call.Args[1].(string)
+		if !ok {
+			return nil, errors.New("Invalid argument")
+		}
+		return nil, node.AddContact(contactName, contactB64Key)
+
+	case "DeleteContact":
+		if len(call.Args) < 1 {
+			return nil, errors.New("Invalid argument count")
+		}
+		contactName, ok := call.Args[0].(string)
+		if !ok {
+			return nil, errors.New("Invalid argument")
+		}
+		return nil, node.DeleteContact(contactName)
+
+	case "GetChannel":
+		if len(call.Args) < 1 {
+			return nil, errors.New("Invalid argument count")
+		}
+		channelName, ok := call.Args[0].(string)
+		if !ok {
+			return nil, errors.New("Invalid argument")
+		}
+		if c, err := node.GetChannel(channelName); err != nil {
+			return nil, err
+		} else {
+			return c, nil
+		}
+
+	case "GetChannels":
+		if chans, err := node.GetChannels(); err != nil {
+			return nil, err
+		} else {
+			return chans, nil
+		}
+
+	case "AddChannel":
+		if len(call.Args) < 2 {
+			return nil, errors.New("Invalid argument count")
+		}
+		channelName, ok := call.Args[0].(string)
+		if !ok {
+			return nil, errors.New("Invalid argument")
+		}
+		channelB64Key, ok := call.Args[1].(string)
+		if !ok {
+			return nil, errors.New("Invalid argument")
+		}
+		return nil, node.AddChannel(channelName, channelB64Key)
+
+	case "DeleteChannel":
+		if len(call.Args) < 1 {
+			return nil, errors.New("Invalid argument count")
+		}
+		channelName, ok := call.Args[0].(string)
+		if !ok {
+			return nil, errors.New("Invalid argument")
+		}
+		return nil, node.DeleteChannel(channelName)
+
+	case "GetProfile":
+		if len(call.Args) < 1 {
+			return nil, errors.New("Invalid argument count")
+		}
+		profileName, ok := call.Args[0].(string)
+		if !ok {
+			return nil, errors.New("Invalid argument")
+		}
+		if profile, err := node.GetProfile(profileName); err != nil {
+			return nil, err
+		} else {
+			return profile, nil
+		}
+
+	case "GetProfiles":
+		if profiles, err := node.GetProfiles(); err != nil {
+			return nil, err
+		} else {
+			return profiles, nil
+		}
+
+	case "AddProfile":
+		if len(call.Args) < 2 {
+			return nil, errors.New("Invalid argument count")
+		}
+		profileName, ok := call.Args[0].(string)
+		if !ok {
+			return nil, errors.New("Invalid argument")
+		}
+		profileEnabled, ok := call.Args[1].(string) //todo: convert this to bool?
+		if !ok {
+			return nil, errors.New("Invalid argument")
+		}
+		enabled, err := strconv.ParseBool(profileEnabled)
+		if err != nil {
+			return nil, errors.New("Invalid bool format")
+		}
+		return nil, node.AddProfile(profileName, enabled)
+
+	case "DeleteProfile":
+		if len(call.Args) < 1 {
+			return nil, errors.New("Invalid argument count")
+		}
+		profileName, ok := call.Args[0].(string)
+		if !ok {
+			return nil, errors.New("Invalid argument")
+		}
+		return nil, node.DeleteProfile(profileName)
+
+	case "LoadProfile":
+		if len(call.Args) < 1 {
+			return nil, errors.New("Invalid argument count")
+		}
+		profileName, ok := call.Args[0].(string)
+		if !ok {
+			return nil, errors.New("Invalid argument")
+		}
+		if p, err := node.LoadProfile(profileName); err != nil {
+			return nil, err
+		} else {
+			return p, nil
+		}
+
+	case "GetPeer":
+		if len(call.Args) < 1 {
+			return nil, errors.New("Invalid argument count")
+		}
+		peerName, ok := call.Args[0].(string)
+		if !ok {
+			return nil, errors.New("Invalid argument")
+		}
+		if peer, err := node.GetPeer(peerName); err != nil {
+			return nil, err
+		} else {
+			return peer, nil
+		}
+
+	case "GetPeers":
+		if peers, err := node.GetPeers(); err != nil {
+			return nil, err
+		} else {
+			return peers, nil
+		}
+
+	case "AddPeer":
+		if len(call.Args) < 1 {
+			return nil, errors.New("Invalid argument count")
+		}
+		peerName, ok := call.Args[0].(string)
+		if !ok {
+			return nil, errors.New("Invalid argument")
+		}
+		peerEnabled, ok := call.Args[1].(string)
+		if !ok {
+			return nil, errors.New("Invalid argument")
+		}
+		peerURI, ok := call.Args[2].(string)
+		if !ok {
+			return nil, errors.New("Invalid argument")
+		}
+		enabled, err := strconv.ParseBool(peerEnabled)
+		if err != nil {
+			return nil, errors.New("Invalid bool format")
+		}
+		return nil, node.AddPeer(peerName, enabled, peerURI)
+
+	case "DeletePeer":
+		if len(call.Args) < 1 {
+			return nil, errors.New("Invalid argument count")
+		}
+		peerName, ok := call.Args[0].(string)
+		if !ok {
+			return nil, errors.New("Invalid argument")
+		}
+		return nil, node.DeletePeer(peerName)
+
+	case "Send":
+		if len(call.Args) < 2 {
+			return nil, errors.New("Invalid argument count")
+		}
+		destName, ok := call.Args[0].(string)
+		if !ok {
+			return nil, errors.New("Invalid argument")
+		}
+		msg, ok := call.Args[1].([]byte)
+		if !ok {
+			return nil, errors.New("Invalid argument")
+		}
+		if len(call.Args) > 2 {
+			rxPubKey, ok := call.Args[2].(bc.PubKey)
+			if !ok {
+				return nil, errors.New("Invalid argument")
+			}
+			return nil, node.Send(destName, msg, rxPubKey)
+		}
+		return nil, node.Send(destName, msg)
+
+	case "SendChannel":
+		if len(call.Args) < 2 {
+			return nil, errors.New("Invalid argument count")
+		}
+		channelName, ok := call.Args[0].(string)
+		if !ok {
+			return nil, errors.New("Invalid argument")
+		}
+		msg, ok := call.Args[1].([]byte)
+		if !ok {
+			return nil, errors.New("Invalid argument")
+		}
+		if len(call.Args) > 2 {
+			rxPubKey, ok := call.Args[2].(bc.PubKey)
+			if !ok {
+				return nil, errors.New("Invalid argument")
+			}
+			return nil, node.SendChannel(channelName, msg, rxPubKey)
+		}
+		return nil, node.SendChannel(channelName, msg)
+
+	default:
+		return node.PublicRPC(call)
+	}
+}
---
+++ policy/common.go
@@ -0,0 +1,84 @@
+package policy
+
+import (
+	"log"
+	"runtime/debug"
+
+	"github.com/awgh/bencrypt/bc"
+	"github.com/awgh/ratnet/api"
+)
+
+var peerTable map[string]*api.PeerInfo
+
+func init() {
+	peerTable = make(map[string]*api.PeerInfo)
+}
+
+// PollServer does a Push/Pull between a local and remote Node
+func PollServer(transport api.Transport, node api.Node, host string, pubsrv bc.PubKey) (bool, error) {
+	// make PeerInfo for this host if doesn't exist
+	if _, ok := peerTable[host]; !ok {
+		peerTable[host] = new(api.PeerInfo)
+	}
+	peer := peerTable[host]
+	//log.Printf("pollServer using peer %+v\n", peer)
+
+	if peer.RoutingPub == nil {
+		rpubkey, err := transport.RPC(host, "ID")
+		if err != nil {
+			log.Println(err.Error())
+			return false, err
+		}
+		rpk, ok := rpubkey.(bc.PubKey)
+		if !ok {
+			log.Println("type assertion failed to bc.PubKey in p2p pollServer")
+			return false, err
+		}
+		peer.RoutingPub = rpk
+	}
+
+	// Pickup Local
+	toRemote, err := node.Pickup(peer.RoutingPub, peer.LastPollLocal)
+	if err != nil {
+		debug.PrintStack()
+		return false, err
+	}
+	log.Println("pollServer Pickup Local result len: ", len(toRemote.Data))
+
+	// Pickup Remote
+	toLocalRaw, err := transport.RPC(host, "Pickup", pubsrv, peer.LastPollRemote)
+	if err != nil {
+		return false, err
+	}
+	toLocal, ok := toLocalRaw.(api.Bundle)
+	if !ok {
+		log.Printf("pollServer type assertion tolocalRaw failed")
+		return false, err
+	}
+	log.Printf("pollServer Pickup Remote len: %d ", len(toLocal.Data))
+
+	peer.TotalBytesRX = peer.TotalBytesRX + int64(len(toLocal.Data))
+
+	// only start tracking time once we start receiving data
+	if peer.TotalBytesTX > 0 {
+		peer.LastPollLocal = toRemote.Time
+	}
+	if peer.TotalBytesRX > 0 {
+		peer.LastPollRemote = toLocal.Time
+	}
+
+	// Dropoff Remote
+	if len(toRemote.Data) > 0 {
+		if _, err := transport.RPC(host, "Dropoff", toRemote); err != nil {
+			return false, err
+		}
+		peer.TotalBytesTX = peer.TotalBytesTX + int64(len(toRemote.Data))
+	}
+	// Dropoff Local
+	if len(toLocal.Data) > 0 {
+		if err := node.Dropoff(toLocal); err != nil {
+			return false, err
+		}
+	}
+	return true, nil
+}
